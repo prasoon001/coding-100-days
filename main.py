1c0128af87de8c16016e47ff22a1696bcc34bf32
Prasoon <prasoon6k@gmail.com>
2022-08-13 23:05:14 +0000

    heds or tail program

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 #Write your code below this line 👇
 #Hint: Remember to import the random module first. 🎲
+import random
 
+rand_hint=random.randint(0,1)
 
+if (rand_hint==0):
+  print("tails")
+else:
+  print("heds")
 
 
 
